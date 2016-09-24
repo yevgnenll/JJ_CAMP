d6419688504b64e085622368557fd1251be3c7b1
seungkwon <yevgnenll@gmail.com>
2016-09-24 19:12:19 +0900

    두가지 방법으로 ul, 추가
    구글링에서 nextSibling을 사옹하는 방법이 있었고,
    강의에서는 body를 찾아 appendChild를 하는 방법을 사용해보았다.
    
    forEach는 cross brwosing issue가 있다
    for와 비교했을때 중간에 멈출 수 있느냐 없느냐의 기준이 있다.

--- LECTURE/DAY01/js/01-fde.js
+++ LECTURE/DAY01/js/01-fde.js
@@ -1,6 +1,9 @@
 (function(){
 
     function init(){
+
+        // var body = document.getElementsByTagName('body').item(0); // XML DOM 방식
+        var body = document.body;
         // 1. 문서에서 단락 요소를 찾아 변수에 참조한다
         var target_p ;
         target_p = document.getElementsByTagName('p').item(0);
@@ -24,6 +27,43 @@
         // ~ 앞에 삽입
         // 목표노드.부모노드.insertBefore(삽입노드, 목표노드);
         target_p.parentNode.insertBefore(headline, target_p);
+
+        // 1. 추가할 노드 만들기
+        var add_ul = document.createElement('ul');
+        var array_list = make_list(add_ul, ['IOT', 'VR', 'AI']);
+
+        target_p.parentNode.insertBefore(array_list, target_p.nextSibling);
+
+        var categories = 'IOT VR IT'.split(' ');
+        // modern ES5 shim JS Library -> 중단 불가
+        categories.forEach(function(item, index){
+            // <li>item</li>
+            var li = document.createElement('li');
+            var li_content = document.createTextNode(item);
+            li.appendChild(li_content);
+            // <ul>요소 내부에 삽입
+
+            add_ul.appendChild(li);
+        });
+        body.appendChild(add_ul);
+
+        // legacy -> 중단 불가
+        var a = 0, l = categories.length;
+        for( ; a<l; a++){
+            console.log(categories[a], a);
+        }
+
+    };
+
+    function make_list(node, lists){
+        for(var i=0; i<lists.length; i++){
+            var parent_li = document.createElement('li');
+            var list_content = document.createTextNode(lists[i]);
+            parent_li.appendChild(list_content);
+            node.appendChild(parent_li);
+        }
+
+        return node;
     };
 
     window.onload = init();

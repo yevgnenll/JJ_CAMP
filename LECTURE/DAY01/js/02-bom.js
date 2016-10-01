2b86015d09607678b4816c2269f293cac3d28a8f
seungkwon <yevgnenll@gmail.com>
2016-10-01 17:04:19 +0900

    backup

--- LECTURE/DAY01/js/02-bom.js
+++ LECTURE/DAY01/js/02-bom.js
@@ -60,7 +60,26 @@
         return type;
     };
 
+    // assign class to html whenever width resize
+    function assign_class_detection(){
+        // html 요소의 class 속성값을 가져온다
+        var html_class = html.getAttribute('class'); 
+        var current_class = detect_device_type();
+        if(!html_class || html_class.pre_class === current_class) {return;}
+        // 기존 클래스 값을 제거한다
+        console.log('---------', assign_class_detection.pre_class);
+        if(html.classList.contains(assign_class_detection.pre_class)){
+            html.classList.remove(assign_class_detection.pre_class);
+        }
+        html.classList.add(detect_device_type());
+        console.log('trycode', html_class);
+        // 현재 설정된 class 값을 기억해두겠다 -> 변수 만드는건 아니다?
+        // memorization
+        assign_class_detection.pre_class = detect_device_type();
+    };
     
+    var init_class = detect_device_type();
+    assign_class_detection.pre_class = init_class;
 
     function class_text(){
     };

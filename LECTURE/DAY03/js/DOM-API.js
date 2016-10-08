1f89fa6064dd2679482f4ead775ae29f911192eb
seungkwon <yevgnenll@gmail.com>
2016-10-08 18:29:49 +0900

    dom을 다루는 방법들에 대해서
    어디서 javascript를 불러올 것이냐
    onload, DOMContentLoaded, 의 차이
    onload는 이미지가 다 뜨고 나오는데 DOMcontentLoaded 는 그렇지 않기
    때문에 더 빠르다
    하지만 script 자체를 body 끝자락으로 끌어오는거싱 훨씬 더 빠르다

--- LECTURE/DAY03/js/DOM-API.js
+++ LECTURE/DAY03/js/DOM-API.js
@@ -4,14 +4,41 @@ console.log('문서에 존재하는 객체: 하이퍼링크', document.links);
 console.log('문서에 존재하는 객체: 네임드앵커', document.anchors);
 console.log('문서에 존재하는 객체: 이미지', document.images);
 console.log('문서에 존재하는 객체: 폼', document.forms[0]);
-console.log('문서에 존재하는 객체: 폼 컨트롤', document.forms[0].elements[0]);
+// console.log('문서에 존재하는 객체: 폼 컨트롤', document.forms[0].elements[0]);
 /* DOM Level 1 
 // W3C 주관 NN, IE 회사가 공통 API를 제작함
 document.getElementById();
 document.getElementsByTagName();
+
+현재 dom level 4
 */
+/*
+ *
+ *
+ */
 
-(function(){
-
-})();
-
+function init(){
+    var html = document.documentElement; // 바로나옴
+    var head = document.head;
+    var body = document.body;
+    console.log(html);
+    console.log(head);
+    console.log(body);
+    body.className = 'dom--legacy-method';
+    // 코드의 실행 시점을 늦춘다.
+    // DOM lv1 방식
+    html = document.getElementsByTagName('html'); // nodelist, object like Array 형태로 나옴
+    head = document.getElementsByTagName('head'); // [<head>]
+    body = document.getElementsByTagName('body'); // [<body>]
+    console.log(html);
+    console.log(head);
+    console.log(body); // 만약 script가 head안에 있다면 null 에러가 나타난다
+    body.setAttribute('class', 'dom-modern-method'); // 에러가 나온다 왜냐하면 array안에 있으므로
+    // body.setAttribute('class', 'dom-modern-method').item(0); // 이 코드가 맞다
+}
+window.onload = init; // load 권장하는 이벤트는 아님 -> 이미지가 다 뜨고나서 나옴
+// DOMcontentLoaded 이벤트 (이미지 로드와 상관 없이 이벤트 발생)
+window.addEventListener('DOMContentLoaded', function(){
+    console.log('DOMContentLoaded');
+}, false);
+// onload보다 DOMcontentLoaded 가 더 빠르기 때문에 DOMContentLoaded 가 먼저 나온다

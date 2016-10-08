fce2276e9db17371c3eb7476afae09fad0b03644
seungkwon <yevgnenll@gmail.com>
2016-10-08 17:48:17 +0900

    dom의 역사

---
+++ LECTURE/DAY03/js/DOM-API.js
@@ -0,0 +1,17 @@
+// level 0 legacy dom
+// 문서 객체에 대한 제한적 접근
+console.log('문서에 존재하는 객체: 하이퍼링크', document.links);
+console.log('문서에 존재하는 객체: 네임드앵커', document.anchors);
+console.log('문서에 존재하는 객체: 이미지', document.images);
+console.log('문서에 존재하는 객체: 폼', document.forms[0]);
+console.log('문서에 존재하는 객체: 폼 컨트롤', document.forms[0].elements[0]);
+/* DOM Level 1 
+// W3C 주관 NN, IE 회사가 공통 API를 제작함
+document.getElementById();
+document.getElementsByTagName();
+*/
+
+(function(){
+
+})();
+

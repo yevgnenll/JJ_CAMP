2c428095b8499adfb60e6c83af97ba73f20b1acf
seungkwon <yevgnenll@gmail.com>
2016-10-29 20:11:41 +0900

    외부에 공개하지 않는 모듈패턴
    global에 Person을 정의해줘야 한다

--- LECTURE/DAY06/js/modules/Person.js
+++ LECTURE/DAY06/js/modules/Person.js
@@ -1,28 +1,47 @@
-
-// 사람을 만들고 싶다
-function Person(
-    name,
-    gender,
-    age,
-    height,
-    weight,
-    hobby,
-    health,
-    sickness){
+// javascript module pattern
+// javascript는 모듈이 존재하지 않았기 때문에 이와같은 방법을 사용하여 모듈생성
+// ES6에서는 공식으로 모듈을 언어 차원에서 지원한다.
+(function(global){
     'use strict';
 
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-    this.hobby = hobby;
-    this.health = health;
-    this.sickness = sickness;
-    this.eat = eat;
-    this.run = run;
-    this.sleep = sleep;
-}
+    // 사람을 만들고 싶다
+    function Person(
+        name,
+        gender,
+        age,
+        height,
+        weight,
+        hobby,
+        health,
+        sickness){
+        'use strict';
+
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.hobby = hobby;
+        this.health = health;
+        this.sickness = sickness;
+        this.eat = eat;
+        this.run = run;
+        this.sleep = sleep;
+    };
+    function eat(what){
+        return this.name + '은' + what + '을 먹었다';
+    };
+    function run(how_much){
+        return this.name + '은' + how_much + '만큼 달렸다.';
+    };
+    function sleep(time){
+        return this.name + '은' + time + '만큼 잤다.';
+    };
+
+    global.Person = Person;
+
+})(this);
+
 
 var cocoaman = new Person(
     '코코아',
@@ -36,17 +55,4 @@ var cocoaman = new Person(
 );
 
 
-// javascript module pattern
-// javascript는 모듈이 존재하지 않았기 때문에 이와같은 방법을 사용하여 모듈생성
-// ES6에서는 공식으로 모듈을 언어 차원에서 지원한다.
-(function(global){
-    function eat(what){
-        return this.name + '은' + what + '을 먹었다';
-    };
-    function run(how_much){
-        return this.name + '은' + how_much + '만큼 달렸다.';
-    };
-    function sleep(time){
-        return this.name + '은' + time + '만큼 잤다.';
-    };
-})(this);
+console.log(cocoaman);

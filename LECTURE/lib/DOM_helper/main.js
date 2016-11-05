d0b9610269fdf5830456061c2c80ac89659b6f6e
seungkwon <yevgnenll@gmail.com>
2016-11-05 21:07:58 +0900

    모듈 정의

---
+++ LECTURE/lib/DOM_helper/main.js
@@ -0,0 +1,11 @@
+/* DOM_helper/main.js */
+// module initialize
+// 조건처리
+// 이미 모듈이 정의된 바가 있다면 정의된 모듈을 사용하라
+// 그렇지 않다면 새로운 빈 모듈을 만들어서 사용한다
+this.DOM_helper = this.DOM_helper || {};
+
+// 작성하고자 하는 모듈
+(function(global){
+
+})(this, this.DOM_helper);
